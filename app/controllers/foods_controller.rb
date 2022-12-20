cbf14129bb03229a78279ab8c3900ab4defda93a
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-12-20 11:21:35 -0600

    Remove testing code for current_user

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -21,18 +21,14 @@ class FoodsController < ApplicationController
 
   # POST /foods or /foods.json
   def create
-    @current_user = User.first
-    food_params[:user_id] = @current_user.id
-    debugger
+
     @food = Food.new(food_params)
     # debugger
     respond_to do |format|
       if @food.save
         format.html { redirect_to food_url(@food), notice: "Food was successfully created." }
-        format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
       end
     end
   end
