09ec043b7f39fd36241d3d7116906f921eeb553d
andym80 <andymenutti@gmail.com>
2022-12-20 13:22:52 -0300

    prepare devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,3 @@ class User < ApplicationRecord
 devise :database_authenticatable, :registerable,
 	   :recoverable, :rememberable, :validatable,
 	   :confirmable
-
-has_many :recipes, foreign_key: 'UserID'
-has_many :foods,
