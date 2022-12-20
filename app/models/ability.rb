ef164b84c72f6a1beadbe35844dbb187bdda76ab
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-12-20 15:48:50 -0600

    Fix linter errors with *RUBOCOP*

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
@@ -28,9 +26,10 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
-    can:manage, :all if user.admin?
+    can :manage, :all if user.admin?
 
-    return unless user.present? #additional permissions for logged in users
+    return unless user.present? # additional permissions for logged in users
+
     can :read, :all
   end
 end
