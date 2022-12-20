a8ca1857f7d5f2a70c0f0a17beea152a2639ee48
andym80 <andymenutti@gmail.com>
2022-12-20 13:33:43 -0300

    create edit.html.rb file : views/passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,3 +3,10 @@ class User < ApplicationRecord
 devise :database_authenticatable, :registerable,
 	   :recoverable, :rememberable, :validatable,
 	   :confirmable
+
+validates :name, presence: true, length: { maximum: 50 }
+validates :password, presence: true, length: { maximum: 50 }
+
+def display_name
+	recipes.limit(5).order(created_at: :desc).map(&:name).join(", ")
+end
