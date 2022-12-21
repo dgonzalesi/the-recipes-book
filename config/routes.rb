ae393e73cbb58d757215790ea97656116be23a88
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-12-21 11:06:13 -0600

    Fix error in redirect from devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'users/registrations' }
   resources :foods
   resources :recipes
+  resources :users, only: [:show]
   root "recipes#index"
 
 end
