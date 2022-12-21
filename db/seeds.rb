ada151eba806e3404324aec85f1b5dfff0b358d8
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-12-21 13:24:45 -0600

    Add edit functionality/ DB: add seed data Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user1 = User.create! :name => 'Juan', :email => 'juan@mail.com', :password => '123456', :password_confirmation => '123456'
+user2 = User.create! :name => 'DanieL', :email => 'daniel@mail.com', :password => '123456', :password_confirmation => '123456'
+user3 = User.create! :name => 'MarIa Ana', :email => 'maria@mail.com', :password => '123456', :password_confirmation => '123456'
+user4 = User.create! :name => 'Pancho', :email => 'pancho@mail.com', :password => '123456', :password_confirmation => '123456'
+user5 = User.create! :name => 'Lucas', :email => 'lucas@mail.com', :password => '123456', :password_confirmation => '123456'
+user6 = User.create! :name => 'joSe', :email => 'jose@mail.com', :password => '123456', :password_confirmation => '123456'
