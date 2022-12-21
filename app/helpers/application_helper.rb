ddc7d234455960a6f6d533d1927365dcfb41f01b
andym80 <andymenutti@gmail.com>
2022-12-20 23:04:20 -0300

    fix application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def total_price_recipe(recipe)
+		total_price = 0
+		recipe_ingredients = RecipeFood.includes([:food]).where(recipe:)
+		recipe_ingredients.each do |ri|
+			total_price += ri.food[:price] * ri[:quantity].to_i
+		end
+		total_price
+	end
 end
