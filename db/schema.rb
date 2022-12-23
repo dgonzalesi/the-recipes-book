a6696b4399bbb09e0ea0db94c1ce0d22c25f1775
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-12-23 15:16:07 -0600

    Add FK after delete a recipe deletes recipe_foods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_23_044621) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_23_210454) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_23_044621) do
 
   add_foreign_key "foods", "users"
   add_foreign_key "recipe_foods", "foods"
-  add_foreign_key "recipe_foods", "recipes"
+  add_foreign_key "recipe_foods", "recipes", on_delete: :cascade
   add_foreign_key "recipes", "users"
 end
