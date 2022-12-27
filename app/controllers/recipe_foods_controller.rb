fcaeebfba74271660a9a5f70a69e6329d4dd844a
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-12-27 11:20:55 -0600

    Delete unused comments

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -16,10 +16,4 @@ class RecipeFoodsController < ApplicationController
 
   def update; end
 
-  # Use callbacks to share common setup or constraints between actions.
-
-  # Only allow a list of trusted parameters through.
-  # def recipe_params
-  #   params.require(:recipe_foods).permit(:name, :preparation_time, :cooking_time, :description, :public)
-  # end
 end
