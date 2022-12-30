9ad9b335ff0c71ca6d5dc64b8661cc1597d62eb2
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-12-30 11:17:58 -0600

    Add create new ingredient view

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,19 +1,41 @@
 class RecipeFoodsController < ApplicationController
+  before_action :set_current_recipe, only: [:new]
+  before_action :set_foods, only: [:new]
   def index
     @recipe_foods = RecipeFood.all
   end
 
-  def new; end
+  def new;
+    @recipe_food = RecipeFood.new
+  end
 
   def show; end
 
-  def create; end
-
+  def create
+    @recipes_list = Recipe.all
+    @foods_list = Food.all
+    @recipe_food = RecipeFood.create(recipe_food_params)
+    debugger
+  end
   def destroy
     @recipe_food = RecipeFood.find(params[:id])
     @recipe_food.destroy
   end
 
   def update; end
+
+  private
+
+  def set_current_recipe
+    @current_recipe = Recipe.find(params[:recipe_id])
+  end
+
+  def set_foods
+    @foods_list = Food.all
+  end
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:quantity, :recipe_id, :food_id)
+  end
 
 end
