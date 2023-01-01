f61cc7ad6193f11f88e79989c5195f8f0dfdd216
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-12-31 22:47:25 -0600

    Add console log to test stimulus present

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,6 +1,10 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+  connect () {
+    console.log(' Hellow Stimulus', this.element)
+  }
+
   open () {
     alert('open')
   }
