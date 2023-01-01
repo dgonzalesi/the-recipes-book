8358b15700261eef1375d9a8b87d25a5c626f0cc
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2023-01-01 17:51:07 -0600

    Add Stimulus JS listener - responsive menu

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,13 +1,22 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+  static targets = [ "buttons", "menuOpen" ]
+
   connect () {
     const width = window.innerWidth
-    console.log(' Hellow Stimulus', this.element, width)
+    const element = this.menuOpenTarget
+    const buttons = this.buttonsTarget
+    if (width > 600) {
+        element.style.display = "none"
+    } else {
+        buttons.style.display = "none"
+    }
   }
 
   open () {
-    alert('open')
+    const element = this.buttonsTarget
+    console.log(element.classList.toggle('my_display'))
   }
 
   close () {
