524863bb0df674b5fbe6c49d93ceeeed4d3b0674
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2023-01-01 18:39:22 -0600

    Add nav menu responsive

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,25 +1,46 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = [ "buttons", "menuOpen" ]
+  static targets = [ "buttons", "menuOpen", "menuClose" ]
 
   connect () {
-    const width = window.innerWidth
-    const element = this.menuOpenTarget
+    let width = window.innerWidth
+    const open = this.menuOpenTarget
     const buttons = this.buttonsTarget
+    const closeBtn = this.menuCloseTarget
     if (width > 600) {
-        element.style.display = "none"
+        open.style.display = "none"
     } else {
         buttons.style.display = "none"
     }
+    addEventListener("resize", () => {
+        width = window.innerWidth
+        if (width > 600) {
+            open.style.display = "none"
+            buttons.style.display = "flex"
+            closeBtn.style.display = "none"
+        } else {
+            buttons.style.display = "none"
+            open.style.display = "inline"
+        }
+    } )
   }
 
   open () {
-    const element = this.buttonsTarget
-    console.log(element.classList.toggle('my_display'))
+    const open = this.menuOpenTarget
+    const closeBtn = this.menuCloseTarget
+    const buttons = this.buttonsTarget
+    closeBtn.style.display = "inline"
+    open.style.display = "none"
+    buttons.style.display = "inline"
   }
 
   close () {
-    alert('close')
+    const open = this.menuOpenTarget
+    const closeBtn = this.menuCloseTarget
+    const buttons = this.buttonsTarget
+    buttons.style.display = "none"
+    closeBtn.style.display = "none"
+    open.style.display = "inline"
   }
 }
